c96f82c9da04292e47059edd547d8e344a69eb88
unknown <werellel@gmail.com>
2020-10-04 23:41:32 +0900

    ex: get post

--- src/com/javalec/ex/PostMethod.java
+++ src/com/javalec/ex/PostMethod.java
@@ -9,19 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
 /**
- * Servlet implementation class HelloWorld
+ * Servlet implementation class PostMethod
  */
-@WebServlet("/HWorld")
-public class HelloWorld extends HttpServlet {
+@WebServlet("/PostMethod")
+public class PostMethod extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloWorld() {
+    public PostMethod() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,20 +29,7 @@ public class HelloWorld extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("doGet");
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.setContentType("text/html; charset=euc-kr");
-		PrintWriter wirter = response.getWriter();
-		
-		wirter.println("<html>");
-		wirter.println("<head>");
-		wirter.println("</head>");
-		wirter.println("<body>");
-		wirter.println("Hello World!");
-		wirter.println("</body>");
-		wirter.println("</html>");
-		
-		wirter.close();
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
@@ -59,7 +44,7 @@ public class HelloWorld extends HttpServlet {
 		wirter.println("<head>");
 		wirter.println("</head>");
 		wirter.println("<body>");
-		wirter.println("<h1>POST 방식 호출, getPost 메소드 호출되었습니다.</h1>");
+		wirter.println("<h1>POST 방식 호출, doPost 메소드 호출되었습니다.</h1>");
 		wirter.println("</body>");
 		wirter.println("</html>");
 		
