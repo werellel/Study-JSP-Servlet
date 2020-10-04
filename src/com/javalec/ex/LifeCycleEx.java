e61af22fdb8944a794571812144455109fa5f4ac
unknown <werellel@gmail.com>
2020-10-04 23:58:36 +0900

    ex: init destory postConstruct preDestory

---
+++ src/com/javalec/ex/LifeCycleEx.java
@@ -0,0 +1,66 @@
+package com.javalec.ex;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LifeCycleEx
+ */
+@WebServlet("/LifeCycleEx")
+public class LifeCycleEx extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LifeCycleEx() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void init() throws ServletException {
+    	System.out.println("init");
+    }
+    
+    @Override
+    public void destroy() {
+    	System.out.println("destroy");
+    }
+    
+    @PostConstruct
+    private void  initPostConstruct() {
+    	System.out.println("initPostConstruct");
+    }
+    
+    @PreDestroy
+    private void destoryPreDestor() {
+		// TODO Auto-generated method stub
+    	System.out.println("destoryPreDestor");
+
+	}
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+    	System.out.println("doGet");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
